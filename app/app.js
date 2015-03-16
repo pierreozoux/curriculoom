80e981afdb66ec35f0f64fafcc0f27e1aab98fdf
Nicolas Toublanc <n.toublanc@gmail.com>
2015-03-16 10:32:49 +0000

    step-02 solution

--- app/app.js
+++ app/app.js
@@ -1,7 +1,36 @@
 (function () {
-    
+
     'use strict';
 
     angular.module('curriculoom', []);
-    
+
+    angular.module('curriculoom').controller('EditUserController', EditUserController);
+
+    function EditUserController($scope) {
+
+        // scope attributes 
+        $scope.firstName;
+        $scope.lastName;
+
+        // scope methods
+        $scope.getFullName = getFullName;
+        $scope.reset = reset;
+
+        init();
+
+        function init() {
+            $scope.reset();
+        }
+
+        function getFullName() {
+            return $scope.firstName + ' ' + $scope.lastName;
+        }
+
+        function reset() {
+            $scope.firstName = 'John';
+            $scope.lastName = 'Smith';
+        }
+
+    }
+
 })();

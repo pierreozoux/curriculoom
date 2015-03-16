d73a5875048575c72bfb1bf7e8d67031a0678c3f
Nicolas Toublanc <n.toublanc@gmail.com>
2015-03-16 10:50:08 +0000

    step 2

--- app/app.js
+++ app/app.js
@@ -10,26 +10,8 @@
 
         // scope attributes 
         $scope.firstName;
-        $scope.lastName;
 
         // scope methods
-        $scope.getFullName = getFullName;
-        $scope.reset = reset;
-
-        init();
-
-        function init() {
-            $scope.reset();
-        }
-
-        function getFullName() {
-            return $scope.firstName + ' ' + $scope.lastName;
-        }
-
-        function reset() {
-            $scope.firstName = 'John';
-            $scope.lastName = 'Smith';
-        }
 
     }
 

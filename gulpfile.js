2df823dcf79328b87d5e9497792f9307b7f288f3
Nicolas Toublanc <n.toublanc@gmail.com>
2015-03-18 10:41:28 +0000

    add html5 middleware

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var historyApiFallback = require('connect-history-api-fallback');
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
-  });
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        livereload: true,
+        middleware: function (connect, opt) {
+            return [historyApiFallback];
+        }
+    });
 });
 
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
-    .pipe(connect.reload());
+    gulp.src('./app/*.html')
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch(['./app/*.html'], ['html']);
 });
 
 gulp.task('default', ['connect', 'watch']);

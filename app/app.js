500662ec58eb86fdbe1643b57b5ae9b9cf992a6e
Nicolas Toublanc <n.toublanc@gmail.com>
2015-03-18 09:47:47 +0000

    integrate ui-router

--- app/app.js
+++ app/app.js
@@ -6,7 +6,30 @@
         'ui.bootstrap',
         'ngMessages',
         'validation.match',
+        'ui.router',
         'curriculoom.profile'
     ]);
+
+
+    angular.module('curriculoom').config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
+        
+        // HTML5 mode
+        $locationProvider.html5Mode(true);
+        
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('dashboard', {
+                url: "/",
+                templateUrl: "modules/dashboard/dashboard.html"
+            }).state('profile-user-edit', {
+                url: "/profile/user-edit",
+                templateUrl: "modules/profile/user.edit.html",
+                controller: 'EditUserController'
+            });
+    });
+
 
 })();
